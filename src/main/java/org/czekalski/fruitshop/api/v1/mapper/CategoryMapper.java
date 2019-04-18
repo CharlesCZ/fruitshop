8620cc92ce6d2f745d9287164c0c61b64491901e
CharlesCZ <karakankaczyn@gmail.com>
2019-04-18 23:22:34 +0200

    using and testing MapStruct

---
+++ src/main/java/org/czekalski/fruitshop/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package org.czekalski.fruitshop.api.v1.mapper;
+
+import org.czekalski.fruitshop.api.v1.model.CategoryDTO;
+import org.czekalski.fruitshop.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE= Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO(Category category);
+
+}
--- src/main/java/org/czekalski/fruitshop/api/v1/model/CategoryDTO.java
+++ src/main/java/org/czekalski/fruitshop/api/v1/model/CategoryDTO.java
@@ -1,5 +1,9 @@
 package org.czekalski.fruitshop.api.v1.model;
 
+
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
