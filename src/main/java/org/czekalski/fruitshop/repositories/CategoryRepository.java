d3b3c0d7f4fbe3fa7b3008f87aed55fa2463353f
CharlesCZ <karakankaczyn@gmail.com>
2019-04-18 22:20:32 +0200

    init

---
+++ src/main/java/org/czekalski/fruitshop/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package org.czekalski.fruitshop.repositories;
+
+import org.czekalski.fruitshop.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
